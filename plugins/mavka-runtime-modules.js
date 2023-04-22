4975d85f3af790d9c661cfbed501433330970805
Davyd Kohut <davyd.kohut@gmail.com>
2023-04-22 17:02:35 +0300

    upd

--- plugins/mavka-runtime-modules.js
+++ plugins/mavka-runtime-modules.js
@@ -1,28 +1,30 @@
-import startupModules from 'mavka/startup-modules';
-import { Buffer } from 'buffer';
+import startupModules from "mavka/startup-modules";
+import { Buffer } from "buffer";
+
+const playgroundStartupModules = startupModules.filter((m) => m.name !== "фс.м");
 
 export default function mavkaStartupModules() {
-  const virtualModuleId = 'virtual:mavka-startup-modules'
-  const resolvedVirtualModuleId = '\0' + virtualModuleId
+  const virtualModuleId = "virtual:mavka-startup-modules";
+  const resolvedVirtualModuleId = "\0" + virtualModuleId;
 
   return {
-    name: 'mavka-startup-modules', // required, will show up in warnings and errors
+    name: "mavka-startup-modules", // required, will show up in warnings and errors
     resolveId(id) {
       if (id === virtualModuleId) {
-        return resolvedVirtualModuleId
+        return resolvedVirtualModuleId;
       }
     },
     load(id) {
       if (id === resolvedVirtualModuleId) {
-        if (!Array.isArray(startupModules)) {
+        if (!Array.isArray(playgroundStartupModules)) {
           throw `Не вдалось завантажити вбудовані модулі.`;
         }
-      
-        const json = JSON.stringify(startupModules);
-        const encoded = Buffer.from(json.replaceAll('\\', '\\\\')).toString("base64");
 
-        return `export default "${encoded}"`
+        const json = JSON.stringify(playgroundStartupModules);
+        const encoded = Buffer.from(json.replaceAll("\\", "\\\\")).toString("base64");
+
+        return `export default "${encoded}"`;
       }
-    },
-  }
+    }
+  };
 }
